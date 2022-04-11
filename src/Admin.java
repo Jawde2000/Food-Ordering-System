a6b92a116f7a9120ae5733bc91fa5244393c9ddb
Zhipeng <1191202464@student.mmu.edu.my>
2022-04-11 23:45:16 +0800

    Admin Prototype structure

---
+++ src/Admin.java
@@ -0,0 +1,33 @@
+public class Admin {
+    private String adminID;
+    private String adminPass;
+
+    Admin() {
+
+    }
+
+    public void viewMenu() {
+
+    }
+
+    public void addFood(String FoodName, String itemPrice, String category, String BHV) {
+
+    }
+
+    public void deleteFood(String ID) {
+
+    }
+
+    public void modifyFood(String ID) {
+
+    }
+
+    public static void main(String args[]) {
+        //Admin interface
+        //from here we are going to find if the admin file is empty, if the system found that admin is new user
+        // the admin is required to type in admin name & admin password x2
+        //if the admin login, he/she will have 4 options to choose which are view menu, 
+        // add menu, delete menu by id, and modify food information by id
+
+    }
+}
