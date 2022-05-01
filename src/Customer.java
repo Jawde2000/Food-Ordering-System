4f5ed2904d6f24aa95057ed691d9ae704419ae70
HinokamiKagura01 <103435386+HinokamiKagura01@users.noreply.github.com>
2022-05-01 18:55:03 +0800

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,37 +1,41 @@
 public class Customer {
   private String tableID;
   private String Remarks;
-}
 
-Customer() {
-  tableID = "";
-  Remarks = "";
-}
+  Customer() {
+   tableID = "";
+   Remarks = "";
+  }
 
-Customer(String tID, String r) {
-  tableID = tID;
-  Remarks = r;
-}
+  Customer(String tID, String r) {
+   tableID = tID;
+   Remarks = r;
+  }
 
-public void confirmFood(String FoodName, String itemPrice, String category, String BHV) {
+  public void viewMenu() {
+    
+  }
 
-}
+  public void confirmFood(String FoodName, String itemPrice, String category, String BHV) {
 
-public void updateFood(String ID) {
+  }
 
-}
+  public void updateFood(String ID) {
 
-public void removeFood(String ID) {
+  }
 
-}
+  public void removeFood(String ID) {
 
-public void addFood(String FoodName, String itemPrice, String category, String BHV) {
+  } 
+
+  public void addFood(String FoodName, String itemPrice, String category, String BHV) {
   
-}
+  }
 
-public static void main(String args[]) {
-  addFood();
-  removeFood();
-  confirmFood();
-  updateFood();
+  public static void main(String args[]) {
+    addFood();
+    removeFood();
+    confirmFood();
+    updateFood();
+  }
 }
